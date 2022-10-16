428aba0a824531b39a77b0587b827be42d946867
User <User@DESKTOP-5HFVUCT>
2022-10-16 22:13:34 +0800

    加入專案檔案。

---
+++ Exec_For01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                string a =  new string('*', i);
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
